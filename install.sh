80fe84913dd1f87fc942ecd0a1586b3b1fa24e7e
Matthew Owens <mowens@redhat.com>
2019-08-21 04:53:18 -0600

    updated install script to setup dev environment

--- install.sh
+++ install.sh
@@ -1,11 +1,35 @@
 #!/bin/sh
-if [ ! -d "./virtv/appinit/lib/" ]; then
-  echo "Building Virtual Environment"
-  virtualenv -p /usr/bin/python3 ./controller/virtv/
-  
-  echo "Installing Dependencies"
-  source ./virtv/appinit/bin/activate
-  pip install -r ./virtv/requirements.txt
+end="\033[0m"
+red="\033[1;31m"
 
-  echo "Done Installing"
+function color {
+  echo "${red}${1}${end}"
+}
+
+if [ ! -d "./virtv/appinit/lib/" ]; then
+  color "Building Virtual Environment"
+  virtualenv -p python3 virtv/appinit
+  
+  color "Installing Dependencies"
+  source virtv/appinit/bin/activate
+  pip install -r virtv/requirements.txt
+
+  color "Linking Packages"
+  pip install --editable packages/auth/
+  pip install --editable packages/backend/
+  pip install --editable packages/cli/
+ 
+  color "Installing npm packages"
+  cd packages/ui
+  yarn install
+  yarn link
+  yarn link @app-init/ui 
+  
+  cd ../frontend
+  yarn install
+  yarn link @app-init/ui 
+
+  cd ../../
+  deactivate  
+  color "Done Installing"
 fi
